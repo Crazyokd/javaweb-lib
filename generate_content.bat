3e9f5e9bdaf23c710e5702b4f4ebe12d670b0f11
Rekord <1324596506@qq.com>
2022-01-11 16:47:14 +0800

    optimize generate_content.bat

--- generate_content.bat
+++ generate_content.bat
@@ -5,14 +5,31 @@
 @REM input head.md
 type README\head.md > README.md
 cd lib
-
-for %%i in (*.*) do (
-    >>..\README.md set /p="- [%%i](lib/%%i)" <nul
-    @rem output new line
-    echo. >> ..\README.md  
+setlocal enabledelayedexpansion
+for %%a in (*.*) do (
+    
+    set t=%%a
+    if "!t:~-4!"==".txt" (
+        set t=!t:~0,-4!.jar
+        >>..\README.md set /p="- [!t!](" <nul
+        type %%a >>..\README.md
+        >>..\README.md set /p=")" <nul
+        @rem output new line
+        echo. >> ..\README.md
+    )  
 )
-echo. >> ..\README.md
+echo. >> ..\README.md  
+
+endlocal
+
 cd %~dp0
 
 @REM input foot.md
 type README\foot.md >> README.md
+
+if exist push.bat (
+    call push.bat
+    echo Push Succeeded!
+) else (
+    echo Please push manually!
+)
